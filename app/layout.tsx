f7cc7207287c329f7878f961469530c8067e2bd6
ari <61393419+eftpmc@users.noreply.github.com>
2024-09-07 00:58:25 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "HHIP",
+  title: "HHIPE",
   authors: { name: "Ari", url: "https://github.com/eftpmc" },
   description: "Hilton Head Island Parking Enforcement",
 };
